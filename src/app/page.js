8703cf0a0a122393a2c06003db15693132167b6e
Sharook <sharookcs@gmail.com>
2025-03-29 09:30:00 +0530

    next js fixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 
 import HomeText from "@/components/HomeText";
 import OurServices from "@/components/OurServices";
